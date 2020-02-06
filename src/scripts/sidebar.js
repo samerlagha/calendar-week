e45bddf36b2ff3a1c151772d8b543fe475f0e47b
unknown <you@example.com>
2020-02-06 16:59:49 +0200

    commit

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -23,27 +23,3 @@ function renderSidebar () {
 renderSidebar();
 
 export { renderSidebar };
-
-
-// function renderSidebar () {
-//   const sidebar = document.querySelector('.sidebar');
-//   const dayHours = [];
-  
-//   for (let hour = 0; hour < 24; hour++) {
-    
-//     const sidebarHour = document.createElement('div');
-//     sidebarHour.classList.add('sidebar__hour');
-    
-//     const sidebarHourText = document.createElement('span');
-//     sidebarHourText.classList.add('sidebar__hour-text');
-    
-//     sidebarHourText.textContent = `${(hour < 10) ? '0' + hour : hour}:00`;
-    
-//     sidebarHour.append(sidebarHourText);
-//     dayHours.push(sidebarHour);
-//   }
-  
-//   sidebar.append(...dayHours);
-// };
-
-// renderSidebar();
