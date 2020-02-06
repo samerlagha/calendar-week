ed10977ec35f3718b56eed39243bd84183113d48
unknown <you@example.com>
2020-02-06 16:09:00 +0200

    commit

---
+++ src/scripts/sidebar.js
@@ -0,0 +1,49 @@
+
+function renderSidebar () {
+  const sidebar = document.querySelector('.sidebar');
+  
+  for (let hour = 0; hour < 24; hour++) {
+    
+    const sidebarHour = document.createElement('div');
+    sidebarHour.classList.add('sidebar__hour');
+    
+    const sidebarHourText = document.createElement('span');
+    sidebarHourText.classList.add('sidebar__hour-text');
+
+    sidebarHourText.textContent = `${(hour < 10) ? '0' + hour : hour}:00`;
+       
+    sidebar.append(sidebarHour);
+    sidebarHour.append(sidebarHourText);
+  }
+
+  const firstHour = sidebar.firstChild;
+  firstHour.classList.add('sidebar__no-display');
+};
+
+renderSidebar();
+
+export { renderSidebar };
+
+
+// function renderSidebar () {
+//   const sidebar = document.querySelector('.sidebar');
+//   const dayHours = [];
+  
+//   for (let hour = 0; hour < 24; hour++) {
+    
+//     const sidebarHour = document.createElement('div');
+//     sidebarHour.classList.add('sidebar__hour');
+    
+//     const sidebarHourText = document.createElement('span');
+//     sidebarHourText.classList.add('sidebar__hour-text');
+    
+//     sidebarHourText.textContent = `${(hour < 10) ? '0' + hour : hour}:00`;
+    
+//     sidebarHour.append(sidebarHourText);
+//     dayHours.push(sidebarHour);
+//   }
+  
+//   sidebar.append(...dayHours);
+// };
+
+// renderSidebar();
---
+++ src/scripts/storage.js
@@ -0,0 +1,16 @@
+import { renderEvents } from './render-events.js'
+
+export { setItemToStorage, getItemFromStorage };
+
+const setItemToStorage = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+const getItemFromStorage = (key) => JSON.parse(localStorage.getItem(key));
+
+
+function onStorageChange(event) {
+    if (event.key === 'events') {
+        renderEvents();
+    }
+}
+window.addEventListener('storage', onStorageChange);
