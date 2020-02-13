02aad9850ebef5ebfef5ccb46082f0bf1ef0e9a7
samerlagha <samer_la@mail.ru>
2020-02-13 18:32:47 +0200

    Update sidebar.js

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -1,21 +1,23 @@
-
-function renderSidebar () {
+//сделать боковую панель
+const  renderSidebar =  ()=> {
   const sidebar = document.querySelector('.sidebar');
-  
+   
+  //hours day
   for (let hour = 0; hour < 24; hour++) {
     
     const sidebarHour = document.createElement('div');
     sidebarHour.classList.add('sidebar__hour');
-    
+    //text hour in sidebar
     const sidebarHourText = document.createElement('span');
     sidebarHourText.classList.add('sidebar__hour-text');
-
+     // if our less then 10 we add '0' before hour
     sidebarHourText.textContent = `${(hour < 10) ? '0' + hour : hour}:00`;
        
     sidebar.append(sidebarHour);
     sidebarHour.append(sidebarHourText);
-  }
-
+  } ;
+  
+   // add class for first hour in sidebar
   const firstHour = sidebar.firstChild;
   firstHour.classList.add('sidebar__no-display');
 };
@@ -23,27 +25,3 @@ function renderSidebar () {
 renderSidebar();
 
 export { renderSidebar };
-
-
-// function renderSidebar () {
-//   const sidebar = document.querySelector('.sidebar');
-//   const dayHours = [];
-  
-//   for (let hour = 0; hour < 24; hour++) {
-    
-//     const sidebarHour = document.createElement('div');
-//     sidebarHour.classList.add('sidebar__hour');
-    
-//     const sidebarHourText = document.createElement('span');
-//     sidebarHourText.classList.add('sidebar__hour-text');
-    
-//     sidebarHourText.textContent = `${(hour < 10) ? '0' + hour : hour}:00`;
-    
-//     sidebarHour.append(sidebarHourText);
-//     dayHours.push(sidebarHour);
-//   }
-  
-//   sidebar.append(...dayHours);
-// };
-
-// renderSidebar();
