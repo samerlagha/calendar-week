856b8b1c483489736609da625ef78f41fae8d093
unknown <you@example.com>
2020-02-13 10:48:20 +0200

    commit

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -1,18 +1,18 @@
-import { renderEvents } from './render-events.js'
+// import { renderEvents } from './render-events.js'
 
-export { setItemToStorage, getItemFromStorage };
+// export { setItemToStorage, getItemFromStorage };
 
-//storage 
+// //storage 
 
-const setItemToStorage = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value));
-}
-const getItemFromStorage = (key) => JSON.parse(localStorage.getItem(key));
+// const setItemToStorage = (key, value) => {
+//     localStorage.setItem(key, JSON.stringify(value));
+// }
+// const getItemFromStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 
-function onStorageChange(event) {
-    if (event.key === 'events') {
-        renderEvents();
-    }
-}
-window.addEventListener('storage', onStorageChange);
+// function onStorageChange(event) {
+//     if (event.key === 'events') {
+//         renderEvents();
+//     }
+// }
+// window.addEventListener('storage', onStorageChange);
