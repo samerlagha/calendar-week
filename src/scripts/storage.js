1899d298771f77ae2e4856d070a7362d5d4fee1d
samerlagha <samer_la@mail.ru>
2020-02-13 18:26:55 +0200

    Update storage.js

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -2,12 +2,17 @@ import { renderEvents } from './render-events.js'
 
 export { setItemToStorage, getItemFromStorage };
 
+//установить пункт для хранения
+
 const setItemToStorage = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
 }
+
+//получить Item из хранилища
 const getItemFromStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 
+//Изменение хранилища
 const onStorageChange = event =>{
     if (event.key === 'events') {
         renderEvents();
