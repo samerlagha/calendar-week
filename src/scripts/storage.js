c534b5e32c95fd5cdbc175b8b31fd977ad18143f
unknown <you@example.com>
2020-02-13 11:30:15 +0200

    commit

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -1,16 +1,16 @@
-// import { renderEvents } from './render-events.js'
+import { renderEvents } from './render-events.js'
 
-// export { setItemToStorage, getItemFromStorage };
+export { setItemToStorage, getItemFromStorage };
 
-// const setItemToStorage = (key, value) => {
-//     localStorage.setItem(key, JSON.stringify(value));
-// }
-// const getItemFromStorage = (key) => JSON.parse(localStorage.getItem(key));
+const setItemToStorage = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+const getItemFromStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 
-// function onStorageChange(event) {
-//     if (event.key === 'events') {
-//         renderEvents();
-//     }
-// }
-// window.addEventListener('storage', onStorageChange);
+function onStorageChange(event) {
+    if (event.key === 'events') {
+        renderEvents();
+    }
+}
+window.addEventListener('storage', onStorageChange);
