c37b9004be646fc87d5c1a5bcdb5b34a09080600
unknown <you@example.com>
2020-02-14 08:27:31 +0200

    commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,6 @@ import { renderSidebar } from './sidebar.js';
 import { displayCurrentWeek, switchWeekForward } from './render-current-week.js';
 import { closePopup, createPopup } from './create-popup.js';
 import { renderEvents, mapEvents } from './render-events.js';
-// import { durationValidation } from './validating-form.js';
 import { editSaveHandler } from './save-event.js';
 import { deleteEvent } from './delete-event.js';
 import { durationValidation } from './validating-form.js';
